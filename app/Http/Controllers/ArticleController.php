049d92dc75971196617e4a1e71aa52e843b6203c
Divrun <denis.03.ermakov@gmail.com>
2024-04-12 15:26:54 +0500

    feat(redis): configured redis for caching. caching is done to retrieve articles and categories.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class ArticleController extends Controller
 {
     public function index(): View
     {
-        $articles = Article::query()->with('categories')->latest()->paginate(6);
-        $categories = Category::all();
+        $articles = Cache::remember('articles', 90, function() {
+            return Article::query()->with('categories')->latest()->paginate(6);
+        });
+        $categories = Cache::remember('categories', 120, function() {
+            return Category::all();
+        });
 
         return view('articles', [
             'articles' => $articles,
